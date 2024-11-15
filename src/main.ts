548597dd01344fec0b470eb70fa9d0eeeaff1f02
Tofu-Xx <1714843907@qq.com>
2024-11-16 03:36:55 +0800

    style: rational api name

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { convertor } from '@/convertor'
 import { generator } from '@/generator'
 
 export interface Options { id?: string, appName?: string }
-export function xxx2({ id = 'sfc2esm', appName = id }: Options = {}) {
+export function useXxx2x({ id = 'sfc2esm', appName = id }: Options = {}) {
   const cache = Object.create(null)
   const opt = { id, appName }
   const h = (tag: string, attrs: Record<string, boolean>, inner: string) => `<${tag} ${Object.keys(attrs).map(k => attrs[k] ? k : '').join(' ')}>${inner}</${tag}>`
@@ -11,7 +11,7 @@ export function xxx2({ id = 'sfc2esm', appName = id }: Options = {}) {
     scr2app: (scriptSource: string, setup = true) => a2b(h('script', { setup }, scriptSource), 'app'),
     tem2render: (templateSource: string) => a2b(h('template', {}, templateSource), 'render'),
     sty2css: (styleSource: string, scoped = false) => a2b(h('style', { scoped }, styleSource), 'css'),
-    sfc2: (sfcSource: string, mount = '#app') => {
+    sfc2x: (sfcSource: string, mount = '#app') => {
       const { isScoped, code: { app, render, css } } = convertor(sfcSource, { cache, ...opt })
       const { initCode, scopeIdCode, createAppCode } = generator({ ...opt, isScoped, mount })
       return {
