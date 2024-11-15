3ed0810faebf4290470bab6e154a89bb1d09469e
Tofu-Xx <1714843907@qq.com>
2024-11-16 00:12:57 +0800

    chore: cache

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,19 @@ import { h } from '@/tools'
 
 export interface Options { id?: string, appName?: string }
 export function createConvertor({ id = 'sfc2esm', appName = id }: Options = {}) {
-  type TagInfo = [string, Record<string, boolean>, string]
-  type CodeType = 'appCode' | 'renderCode' | 'cssCode'
-  const x2x = (c: CodeType, ...t: TagInfo) => convertor(h(...t), { id, appName })[c]
+  const opt = { id, appName }
+  const a2b = (source: string, b: 'appCode' | 'renderCode' | 'cssCode') => convertor(source, opt)[b]
   return {
-    script2app: (scriptSource: string, setup = true) => x2x('appCode', 'script', { setup }, scriptSource),
-    template2render: (templateSource: string) => x2x('renderCode', 'template', {}, templateSource),
-    style2css: (styleSource: string, scoped = false) => x2x('cssCode', 'style', { scoped }, styleSource),
+    script2app: (scriptSource: string, setup = true) => a2b(h('script', { setup }, scriptSource), 'appCode'),
+    template2render: (templateSource: string) => a2b(h('template', {}, templateSource), 'renderCode'),
+    style2css: (styleSource: string, scoped = false) => a2b(h('style', { scoped }, styleSource), 'cssCode'),
+    sfc2app: (sfcSource: string) => a2b(sfcSource, 'appCode'),
+    sfc2render: (sfcSource: string) => a2b(sfcSource, 'renderCode'),
+    sfc2css: (sfcSource: string) => a2b(sfcSource, 'cssCode'),
     sfc2esm: (sfcSource: string, mount = '#app') => {
-      const { isScoped, appCode, renderCode } = convertor(sfcSource, { id, appName })
-      const { initCode, createAppCode, scopeIdCode } = generator({ id, appName, isScoped, mount })
+      const { isScoped, appCode, renderCode } = convertor(sfcSource, opt)
+      const { initCode, createAppCode, scopeIdCode } = generator({ ...opt, isScoped, mount })
       return [initCode, appCode, renderCode, scopeIdCode, createAppCode].join('\n')
     },
-    sfc2mult: (sfcSource: string) => convertor(sfcSource, { id, appName }),
   }
 }
