d0756c1c68f2e99b9e6edb04c9962f4e7265c217
Tofu-Xx <1714843907@qq.com>
2026-03-19 01:49:43 +0800

    refactor!: drop old API

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,17 @@
-import { convertor } from '@/convertor'
 import { generator } from '@/generator'
+import { compilerSfc } from './compiler'
+import { scriptTransformer, stylesTransformer, templateTransformer } from './transformer'
 
-export interface Options { id?: string, appName?: string }
-export function useXxx2x({ id = 'sfc2esm', appName = id }: Options = {}) {
-  const cache = Object.create(null)
-  const opt = { id, appName }
-  const h = (tag: string, attrs: Record<string, boolean>, inner: string) =>
-    `<${tag} ${Object.keys(attrs).map(k => attrs[k] ? k : '').join(' ')}>${inner}</${tag}>`
-  const a2b = (source: string, b: 'app' | 'render' | 'css') =>
-    convertor(source, { cache, ...opt }).code[b]
+export function sfc2esm(sfcSource: string, { id = 'sfc2esm', appName = id, mount = '#app' }: Options) {
+  const { isScoped, compiled: c } = compilerSfc(sfcSource, id)
+  const appCode = scriptTransformer(c.sfcAppBlock, appName)
+  const renderCode = templateTransformer(c.sfcTemplateCompileResults, appName)
+  const cssCode = stylesTransformer(c.sfcStyleCompileResultsList)
+  const { initCode, scopeIdCode, createAppCode } = generator({ id, appName, isScoped, mount })
   return {
-    scr2app: (scriptSource: string, setup = true) =>
-      a2b(h('script', { setup }, scriptSource), 'app'),
-    tem2render: (templateSource: string) =>
-      a2b(h('template', {}, templateSource), 'render'),
-    sty2css: (styleSource: string, scoped = false) =>
-      a2b(h('style', { scoped }, styleSource), 'css'),
-    sfc2x: (sfcSource: string, mount = '#app') => {
-      const { isScoped, code: { app, render, css } } = convertor(sfcSource, { cache, ...opt })
-      const { initCode, scopeIdCode, createAppCode } = generator({ ...opt, isScoped, mount })
-      return {
-        esm: [initCode, app, render, scopeIdCode, createAppCode].join('\n'),
-        app,
-        render,
-        css,
-      }
-    },
+    esmCode: [initCode, appCode, renderCode, scopeIdCode, createAppCode].join('\n'),
+    cssCode,
   }
 }
+
+export interface Options { id?: string, appName?: string, mount?: string }
